05302ab1d6a98925dc0b06197dcee43ef9db0db2
Miriam Mendoza Zurita <miriammz@iota21.aulas.gsyc.es>
2014-11-17 10:30:18 +0100

    añadido envio de ACK

--- client.py
+++ client.py
@@ -5,26 +5,49 @@ Programa cliente que abre un socket a un servidor
 """
 
 import socket
+import sys
 
 # Cliente UDP simple.
 
-# Dirección IP del servidor.
-SERVER = 'localhost'
-PORT = 6001
+if len(sys.argv) != 3:
+    print "Usage: python client.py method receiver@IP:SIPport"
+    sys.exit()
 
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+# Dirección IP del servidor.
+METODO = ['INVITE', 'ACK', 'BYE']
+METODO = sys.argv[1]
+arg1 = sys.argv[2].split("@")
+LOGIN = arg1[0]
+arg2 = arg1[1].split(":")
+IP = arg2[0]
+PORT = int(arg2[1])
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-my_socket.connect((SERVER, PORT))
+my_socket.connect((IP, PORT))
+
+#creamos LINE segun el tipo del metodo que pasamos como argumento
+if METODO == "INVITE":
+	LINE = "INVITE sip:" + LOGIN + "@" + IP + " SIP/2.0" + '\r\n'
+elif METODO == "BYE":
+	LINE = "BYE sip:" + LOGIN + "@" + IP + " SIP/2.0" + '\r\n'
 
 print "Enviando: " + LINE
-my_socket.send(LINE + '\r\n')
-data = my_socket.recv(1024)
+my_socket.send(LINE + '\r\n') #poner bien los \r\n
 
-print 'Recibido -- ', data
+try:
+	data = my_socket.recv(1024)
+	print 'Recibido -- ', data
+except socket.error:
+	sys.exit("Error: No server listening at " + IP + " port " + str(PORT))
+
+if METODO == "INVITE":
+	datos = data.split(" ")
+	if int(datos[1]) == 100 and int(datos[5]) == 180 and int(datos[9]) == 200:
+	    LINE = "ACK sip:" + LOGIN + "@" + IP + " SIP/2.0"
+	    my_socket.send(LINE + '\r\n')
+
 print "Terminando socket..."
 
 # Cerramos todo
