43af15e483ba74a28df611681eaf1229be040e4d
Miriam Mendoza Zurita <miriammz@iota21.aulas.gsyc.es>
2014-11-17 10:31:07 +0100

    añadidos codigos de respuesta y excepcion

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ Clase (y programa principal) para un servidor de eco en UDP simple
 """
 
 import SocketServer
+import sys
 
 
 class EchoHandler(SocketServer.DatagramRequestHandler):
@@ -14,18 +15,37 @@ class EchoHandler(SocketServer.DatagramRequestHandler):
 
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
-        self.wfile.write("Hemos recibido tu peticion")
+        self.wfile.write("Hemos recibido tu peticion") #no hay que imprimirlo
+        line = self.rfile.read()
+        line2 = line.split(" ")
+        if line2[0] == "INVITE":
+            self.wfile.write("SIP/2.0 100 TRYING" + '\r\n' + "SIP/2.0 180 RING" 
+                                + '\r\n' + "SIP/2.0 200 OK" + '\r\n\r\n')
+        elif line2[0] == "BYE":
+            self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
+        elif line2[0] == "ACK":
+            self.wfile.write("SIP/2.0 200 OK" + '\r\n\r\n')
+        elif line2[0] != "INVITE" and line2[0] != "BYE" and line2[0] != "ACK":
+            self.wfile.write("SIP/2.0 405 Method Not Allowed")
+        else
+            self.wfile.write("SIP/2.0 400 Bad Request")
+        # Leyendo línea a línea lo que nos envía el cliente
+        print "El cliente nos manda " + line
         while 1:
-            # Leyendo línea a línea lo que nos envía el cliente
-            line = self.rfile.read()
-            print "El cliente nos manda " + line
-
             # Si no hay más líneas salimos del bucle infinito
+            print "hola"
             if not line:
                 break
 
 if __name__ == "__main__":
+    # Comprobamos que introducimos el numero correcto de parametros
+    if len(sys.argv) != 4:
+        sys.exit("Usage: python server.py IP port audio_file")
+
     # Creamos servidor de eco y escuchamos
-    serv = SocketServer.UDPServer(("", 6001), EchoHandler)
-    print "Lanzando servidor UDP de eco..."
+    IP = sys.argv[1]
+    PUERTO = int(sys.argv[2])
+    FICH_AUDIO = sys.argv[3]
+    serv = SocketServer.UDPServer(("", PUERTO), EchoHandler)
+    print "Listening..."
     serv.serve_forever()
